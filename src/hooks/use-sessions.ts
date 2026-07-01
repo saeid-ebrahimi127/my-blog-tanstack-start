601862fa24434a12dd7c164ab06a0a4999108582
saeid ebrahimi <saeid.ebrahimi127@gmail.com>
2026-07-01 17:33:32 +0330

    fix(use-sessions): handle currentSession error separately and sort sessions by date desc

--- src/hooks/use-sessions.ts
+++ src/hooks/use-sessions.ts
@@ -7,17 +7,24 @@ export const useSessions = () => {
   return useQuery({
     queryKey: sessionsQueryKey,
     async queryFn({ signal }) {
-      const [{ data, error }, { data: current }] = await Promise.all([
+      const [sessionsList, currentSession] = await Promise.all([
         authClient.listSessions({ fetchOptions: { signal } }),
         authClient.getSession({ fetchOptions: { signal } }),
       ])
 
-      if (error) throw new Error('failed to get sessions.')
+      if (sessionsList.error) throw new Error('failed to get sessions.')
+      if (currentSession.error)
+        throw new Error('failed to get current session.')
 
-      return data.map((session) => ({
-        ...session,
-        isCurrent: session.token === current?.session.token,
-      }))
+      return sessionsList.data
+        .map((session) => ({
+          ...session,
+          isCurrent: session.token === currentSession.data?.session.token,
+        }))
+        .sort(
+          (a, b) =>
+            new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime(),
+        )
     },
     staleTime: Infinity,
     refetchOnMount: true,
