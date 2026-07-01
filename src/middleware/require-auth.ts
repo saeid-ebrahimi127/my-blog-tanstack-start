9b0856dc3955c5db87c6766ec6daf25eae925041
saeid ebrahimi <saeid.ebrahimi127@gmail.com>
2026-07-01 08:34:04 +0330

    feat: add requireAuthMiddleware for server function auth redirects

--- src/middleware/require-auth.ts
+++ src/middleware/require-auth.ts
@@ -1,4 +1,5 @@
 import { getUser } from '#/lib/user.server'
+import { redirect } from '@tanstack/react-router'
 import { createMiddleware } from '@tanstack/react-start'
 
 export const requireAuthApiMiddleware = createMiddleware({
@@ -7,6 +8,18 @@ export const requireAuthApiMiddleware = createMiddleware({
   const user = await getUser()
 
   if (!user) return Response.json({}, { status: 401 })
+
+  return next({ context: { user: { id: user.id, image: user.image } } })
+})
+
+export const requireAuthMiddleware = createMiddleware({
+  type: 'function',
+}).server(async ({ next }) => {
+  const user = await getUser()
+
+  if (!user) {
+    throw redirect({ to: '/login', replace: true })
+  }
 
   return next({ context: { user: { id: user.id, image: user.image } } })
 })
