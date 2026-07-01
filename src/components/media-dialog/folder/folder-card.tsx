3509bf282752bc3eab334fd5b811684efff41b47
saeid ebrahimi <saeid.ebrahimi127@gmail.com>
2026-07-01 19:34:22 +0330

    feat(folder-list): add refresh button and disable dropdown during refetch

--- src/components/media-dialog/folder/folder-card.tsx
+++ src/components/media-dialog/folder/folder-card.tsx
@@ -9,26 +9,36 @@ import type { Folder } from '#/serverfn/folder'
 import { EditIcon, FolderIcon } from 'lucide-react'
 import { useState } from 'react'
 
-export const FolderCard = ({ folder }: { folder: Folder }) => {
+export const FolderCard = ({
+  folder,
+  foldersRefetching,
+}: {
+  folder: Folder
+  foldersRefetching: boolean
+}) => {
   const [menuOpen, setMenuOpen] = useState(false)
 
-  return (
+  const card = (
+    <button
+      type="button"
+      onContextMenu={(e) => {
+        e.preventDefault()
+        setMenuOpen(true)
+      }}
+      className="hover:border-primary focus:border-primary flex w-28 flex-col items-center gap-2 rounded-xl border p-4 transition-colors duration-200 focus:outline-none"
+    >
+      <FolderIcon className="size-10 shrink-0 fill-yellow-400 text-yellow-400" />
+      <span className="line-clamp-2 w-full px-2 text-center text-xs">
+        {folder.name}
+      </span>
+    </button>
+  )
+
+  return foldersRefetching ? (
+    card
+  ) : (
     <DropdownMenu modal={false} open={menuOpen} onOpenChange={setMenuOpen}>
-      <DropdownMenuTrigger asChild>
-        <button
-          type="button"
-          onContextMenu={(e) => {
-            e.preventDefault()
-            setMenuOpen(true)
-          }}
-          className="hover:border-primary focus:border-primary flex w-28 flex-col items-center gap-2 rounded-xl border p-4 transition-colors duration-200 focus:outline-none"
-        >
-          <FolderIcon className="size-10 shrink-0 fill-yellow-400 text-yellow-400" />
-          <span className="line-clamp-2 w-full px-2 text-center text-xs">
-            {folder.name}
-          </span>
-        </button>
-      </DropdownMenuTrigger>
+      <DropdownMenuTrigger asChild>{card}</DropdownMenuTrigger>
       <DropdownMenuContent
         align="start"
         side="left"
--- src/components/media-dialog/folder/folder-list.tsx
+++ src/components/media-dialog/folder/folder-list.tsx
@@ -1,26 +1,68 @@
 import { useFolderCtx } from '#/components/media-dialog/body'
 import { FolderCard } from '#/components/media-dialog/folder/folder-card'
 import { FolderListSkeleton } from '#/components/media-dialog/folder/folder-list-skeleton'
+import { Button } from '#/components/ui/button'
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from '#/components/ui/tooltip'
 import { useFolders } from '#/hooks/use-folders'
+import { Loader2Icon, RefreshCwIcon } from 'lucide-react'
 
 export const FolderList = () => {
   const { parentFolderId } = useFolderCtx()
 
-  const { isPending, isError, data: folders } = useFolders({ parentFolderId })
+  const {
+    isPending,
+    isError,
+    data: folders,
+    isRefetching,
+    refetch,
+  } = useFolders({ parentFolderId })
 
   if (isPending) return <FolderListSkeleton />
 
   if (isError) return null
 
-  return folders.length > 0 ? (
-    <div className="flex flex-wrap gap-4">
-      {folders.map((folder) => {
-        return <FolderCard key={folder.id} folder={folder} />
-      })}
+  return (
+    <div className="space-y-4">
+      <Tooltip>
+        <TooltipTrigger asChild>
+          <Button
+            type="button"
+            disabled={isRefetching}
+            onClick={() => {
+              refetch()
+            }}
+            size="icon"
+          >
+            {isRefetching ? (
+              <Loader2Icon className="animate-spin" />
+            ) : (
+              <RefreshCwIcon />
+            )}
+          </Button>
+        </TooltipTrigger>
+        <TooltipContent>بروزرسانی</TooltipContent>
+      </Tooltip>
+      {folders.length > 0 ? (
+        <div className="flex flex-wrap gap-4">
+          {folders.map((folder) => {
+            return (
+              <FolderCard
+                foldersRefetching={isRefetching}
+                key={folder.id}
+                folder={folder}
+              />
+            )
+          })}
+        </div>
+      ) : (
+        <p className="text-muted-foreground text-xs italic">
+          هیچ پوشه ای یافت نشد.
+        </p>
+      )}
     </div>
-  ) : (
-    <p className="text-muted-foreground text-xs italic">
-      هیچ پوشه ای یافت نشد.
-    </p>
   )
 }
